8a351d8227eb7a3190e115849686e4f3a24da858
Inrepublica <duvalmickael@gmail.com>
2020-05-01 23:12:25 +0200

    Mise a jour du batch
    Mise a jour suite à la nouvelle arborescence.

--- outils/copy_translation.bat
+++ outils/copy_translation.bat
@@ -4,20 +4,11 @@ mode con cols=80 lines=16
 color 0F
 
 :: Modifier les valeurs source et cible
-set source="C:\Users\__UTILISATEUR__\Desktop\Capitalism_Lab\target"
+set source="C:\Users\__UTILISATEUR__\Desktop\Capitalism_Lab_ENG_to_FR\target\Release"
 set cible="C:\Users\__UTILISATEUR__\AppData\Local\Capitalism Lab"
 
 :: Copie des fichiers
-xcopy %source%\*.txt %cible%\Translate\ /Y
-xcopy %source%\guide\*.txt %cible%\text\guide\ /Y
-xcopy %source%\tutorial\*.txt %cible%\tutorial\ /Y
-xcopy %source%\tutorial\script\*.txt %cible%\tutorial\script\ /Y
-xcopy %source%\scenario\*.txt %cible%\scenario\ /Y
-xcopy %source%\scenario_dlc\CityEcoSim\*.txt %cible%\scenario_dlc\CityEcoSim\ /Y
-xcopy %source%\scenario_dlc\Subsidiary\*.txt %cible%\scenario_dlc\Subsidiary\ /Y
-xcopy %source%\challenge\*.txt %cible%\challenge\ /Y
-xcopy %source%\script\*.txt %cible%\script\ /Y
-
+xcopy %source%\*.* %cible%\ /Y /S
 
 Pause
 exit
