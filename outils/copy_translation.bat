ad1ba4e4a748658b2c7afc5c6fd8195fe999a079
Inrepublica <duvalmickael@gmail.com>
2020-05-01 23:23:32 +0200

    Mise a jour du batch
    Ajout du %USERNAME%, plus besoin de l'éditer si on a une configuration par défaut.

--- outils/copy_translation.bat
+++ outils/copy_translation.bat
@@ -4,8 +4,8 @@ mode con cols=80 lines=16
 color 0F
 
 :: Modifier les valeurs source et cible
-set source="C:\Users\__UTILISATEUR__\Desktop\Capitalism_Lab_ENG_to_FR\target\Release"
-set cible="C:\Users\__UTILISATEUR__\AppData\Local\Capitalism Lab"
+set source="C:\Users\%USERNAME%\Desktop\Capitalism_Lab_ENG_to_FR\target\Release"
+set cible="C:\Users\%USERNAME%\AppData\Local\Capitalism Lab"
 
 :: Copie des fichiers
 xcopy %source%\*.* %cible%\ /Y /S
