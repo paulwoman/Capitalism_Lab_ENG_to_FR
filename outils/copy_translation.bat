05d26fa5628c32b2fcc88e4ac9c9199b77f8c242
paulwoman <36502605+paulwoman@users.noreply.github.com>
2020-05-03 11:09:17 +0200

    Mise à jour par défaut

--- outils/copy_translation.bat
+++ outils/copy_translation.bat
@@ -4,7 +4,7 @@ mode con cols=80 lines=16
 color 0F
 
 :: Modifier les valeurs source et cible
-set source="C:\Users\%USERNAME%\Desktop\Capitalism_Lab_ENG_to_FR\target\Release"
+set source="C:\Users\%USERNAME%\Documents\Capitalism_Lab_ENG_to_FR\target\Release"
 set cible="C:\Users\%USERNAME%\AppData\Local\Capitalism Lab"
 
 :: Copie des fichiers
