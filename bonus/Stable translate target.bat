a6b87d224d628fb6d20646624aede2691e021d3f
paulwoman <36502605+paulwoman@users.noreply.github.com>
2020-05-11 11:10:00 +0200

    Create Stable translate target.bat

---
+++ bonus/Stable translate target.bat
@@ -0,0 +1,14 @@
+@echo off
+title Copy Traduction
+mode con cols=80 lines=16
+color 0F
+
+:: Modifier les valeurs source et cible
+set source="C:\Users\%USERNAME%\Documents\Capitalism_Lab_ENG_to_FR\target"
+set cible="C:\Users\%USERNAME%\AppData\Local\Capitalism Lab"
+
+:: Copie des fichiers
+xcopy %source%\Release\Translate\translate.txt %cible%\Translate\ /Y
+
+Pause
+exit
