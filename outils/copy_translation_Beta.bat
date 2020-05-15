6220c875792eeab9b032f7347ef4446db7352718
paulwoman <36502605+paulwoman@users.noreply.github.com>
2020-05-15 19:19:53 +0200

    Update copy_translation_Beta.bat

--- outils/copy_translation_Beta.bat
+++ outils/copy_translation_Beta.bat
@@ -8,24 +8,11 @@ set source="C:\Users\%USERNAME%\Documents\Capitalism_Lab_ENG_to_FR\target"
 set cible="C:\Users\%USERNAME%\AppData\Local\Capitalism Lab Post-Release Beta"
 
 :: Copie des fichiers
+xcopy %source%\*.* %cible%\ /Y /S
 xcopy %source%\BETA\Translate\*.txt %cible%\Translate\ /Y
-xcopy %source%\Release\challenge\*.txt %cible%\challenge\ /Y
-xcopy %source%\Release\font\*.html %cible%\font\ /Y
-xcopy %source%\Release\text\guide\*.txt %cible%\text\guide\ /Y
-xcopy %source%\Release\MOD\*.txt %cible%\MOD\ /Y
-xcopy %source%\Release\MOD_kit\*.txt %cible%\MOD_kit\*\ /Y
-xcopy %source%\Release\script\*.txt %cible%\script\ /Y/S
-xcopy %source%\Release\scenario\*.txt %cible%\scenario\ /Y
-xcopy %source%\Release\scenario_dlc\*.txt %cible%\scenario_dlc\ /Y/S
-xcopy %source%\Release\Translate\translate.txt %cible%\Translate\ /Y
-xcopy %source%\Release\Translate\t_basic.txt %cible%\Translate\ /Y
-xcopy %source%\Release\tutorial\*.txt %cible%\tutorial\ /Y/S
-
-
-
-
-
-
 
 Pause
 exit
+
+
+
