240b515971b4bf950279586e18d272f4f317a5fd
paulwoman <36502605+paulwoman@users.noreply.github.com>
2020-05-18 00:29:33 +0200

    Images
    Trauduction de quelques parties en français

--- bonus/Batch/Stable Capitalism.bat
+++ bonus/Batch/Stable Capitalism.bat
@@ -4,11 +4,11 @@ mode con cols=80 lines=16
 color 0F
 
 :: Modifier les valeurs source et cible
-set source="C:\Users\%USERNAME%\Documents\Capitalism_Lab_ENG_to_FR\target\Release"
+set source="C:\Users\%USERNAME%\Documents\Capitalism_Lab_ENG_to_FR\target\"
 set cible="C:\Users\%USERNAME%\AppData\Local\Capitalism Lab"
 
 :: Copie des fichiers
-xcopy %source%\*.* %cible%\ /Y /S
-
+xcopy %source%\Release\*.* %cible%\ /Y /S
+xcopy %source%\image\*.* %cible%\ /Y /S
 Pause
 exit
